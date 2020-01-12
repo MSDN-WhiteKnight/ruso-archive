46cad267245bec2a7069dc6dec8574128eed5694
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-01-12 23:23:12 +0500

    Fix build script

--- build.cmd
+++ build.cmd
@@ -1,4 +1,7 @@
-tools\src\ArchiveLoader\bin\Debug\ArchiveLoader.exe generate
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" tools\src\RuSoTools.sln
+cd tools\src\ArchiveLoader\bin\Debug\
+ArchiveLoader.exe generate
+cd ..\..\..\..\..\
 %DOCFX_PATH%\docfx.exe .\docfx.json
 %DOCFX_PATH%\docfx.exe serve .\html
 pause
