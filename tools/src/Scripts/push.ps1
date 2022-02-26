a2dfe9a4a494cb425d243019c84faebf6193491f
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-02-26 19:45:12 +0500

    Add push script

---
+++ tools/src/Scripts/push.ps1
@@ -0,0 +1,8 @@
+param ($pathspec = '*')
+param ($message = 'Update data (auto)')
+
+git config user.name 'Workhorse'
+git config user.email 'workhorse@knight.ru'
+git add $pathspec
+git commit -m $message
+git push
