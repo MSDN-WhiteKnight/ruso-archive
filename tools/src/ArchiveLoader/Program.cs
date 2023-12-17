066966ee05bc0883e9a0a7a1b0990261d50eed6d
SmallSoft <vadim--111@yandex.ru>
2023-12-17 17:08:26 +0500

    Bump starting point to 13000

--- tools/src/ArchiveLoader/Program.cs
+++ tools/src/ArchiveLoader/Program.cs
@@ -34,7 +34,7 @@ namespace ArchiveLoader
 
         static void LoadDataMarkdown()
         {
-            const int StartingPoint = 12500;
+            const int StartingPoint = 13000;
             string site = "ru.meta.stackoverflow.com";
             string datadir = "..\\..\\..\\..\\data\\" + site + "\\";
             string postsdir = Path.Combine(datadir, "posts-raw\\");
