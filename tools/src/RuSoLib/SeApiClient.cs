8abd72054294c65d9669344b932561a0496e62bc
SmallSoft <vadim--111@yandex.ru>
2024-08-16 17:41:31 +0500

    Add user agent header

--- tools/src/RuSoLib/SeApiClient.cs
+++ tools/src/RuSoLib/SeApiClient.cs
@@ -24,6 +24,8 @@ namespace RuSoLib
 
         public string DoRequest(string url)
         {
+            wc.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0";
+
             byte[] bytes = wc.DownloadData(url);
             byte[] bytes_decompressed;
             using (MemoryStream original = new MemoryStream(bytes))
